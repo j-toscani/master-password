ba47b8c57b27ee3d6a60178768b9ddcd88210098
j-toscani <julian_toscani@gmx.de>
2019-10-02 14:05:16 +0200

    add forms

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 // create webserver
 const { get } = require("./lib/commands");
 const url = require("url");
+const fs = require("fs");
 
 const http = require("http");
 
 //create a server object:
 const server = http.createServer(function(request, response) {
-  if (request.url === "/favicon.ico") {
+  const { pathname } = url.parse(request.url);
+  if (pathname === "/favicon.ico") {
+    response.writeHead(404);
     return response.end();
+  } else if (pathname === "/") {
+    response.writeHead(200, { ContentType: "text/html" });
+    const content = fs.readFileSync("src/view/index.html", "utf-8");
+    return response.end(content);
   }
   try {
-    const secret = get(
-      "1234",
-      url.parse(request.url, false, true).pathname.slice(1)
-    );
+    const secret = get("1234", pathname.slice(1));
     response.write(secret);
   } catch (error) {
     response.write("Can not read secret");
